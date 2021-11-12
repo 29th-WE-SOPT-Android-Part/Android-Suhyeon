d4ff62a6bd7a3ccc7fac9a04ad1e7c1b949cc5f4
onpyeong <gsh447114@gmail.com>
2021-11-12 21:10:34 +0900

    [ADD] add UserService, UserServiceCreator

---
+++ app/src/main/java/org/sopt/sopthub/data/remote/api/UserServiceCreator.kt
@@ -0,0 +1,15 @@
+package org.sopt.sopthub.data.remote.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object UserServiceCreator {
+    private const val BASE_URL = "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api/"
+
+    private val retrofit : Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val userService : UserService = retrofit.create(UserService::class.java)
+}
