ae8bf84b6f3993e5e29739e1aed75b5e474afba1
onpyeong <gsh447114@gmail.com>
2021-11-12 21:09:52 +0900

    [ADD] add Res, Req Data

---
+++ app/src/main/java/org/sopt/sopthub/data/remote/model/response/ResSignUpData.kt
@@ -0,0 +1,14 @@
+package org.sopt.sopthub.data.remote.model.response
+
+data class ResSignUpData(
+    val status : Int,
+    val success: Boolean,
+    val message: String,
+    val data: Data
+) {
+    data class Data (
+        val id : Int,
+        val name :String,
+        val email: String
+    )
+}
